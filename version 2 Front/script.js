b5f15cd14535ac95c3c6579b399e3dd539b4ec9c
Julián A. Rojas <80870138+JulianARojasBernal@users.noreply.github.com>
2022-05-15 01:55:13 -0500

    archivos .js y .css
    Carga de los archivos de la segunda version del Front

---
+++ version 2 Front/script.js
@@ -0,0 +1,32 @@
+var $cell = $('.card');
+
+//ABRIR Y CERRAR TARJETA CON EL CLICK
+$cell.find('.js-expander').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  if ($thisCell.hasClass('is-collapsed')) {
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+    
+    if ($cell.not($thisCell).hasClass('is-inactive')) {
+      
+    } else {
+      $cell.not($thisCell).addClass('is-inactive');
+    }
+
+  } else {
+    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+    $cell.not($thisCell).removeClass('is-inactive');
+  }
+});
+
+//CERRAR TARJETA CON EL CLICK
+$cell.find('.js-collapser').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  $cell.not($thisCell).removeClass('is-inactive');
+
+});
